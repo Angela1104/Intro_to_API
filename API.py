fb39b8959cccbc9bd8786cbb381c6704bceb9db4
Karen Angela Realubit <142026862+Angela1104@users.noreply.github.com>
2024-11-14 20:49:26 +0800

    Can get the book by specific book_id

--- API.py
+++ API.py
@@ -30,9 +30,35 @@ books = [
     }
 ]
 
+def find_book(book_id):
+    return next((book for book in books if book["id"] == book_id), None)
+
 @app.route("/api/books", methods=["GET"])
 def get_books():
     return jsonify({"success": True, "data": books, "total": len(books)}), HTTPStatus.OK
+
+@app.route("/api/books/<int:book_id>", methods=["GET"])
+def get_book(book_id):
+    book = find_book(book_id)
+    if book is None:
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": "Book not found",
+                }
+            ),
+            HTTPStatus.NOT_FOUND,
+        )
+    return (
+        jsonify(
+            {
+                "success": True,
+                "data": book,
+            }
+        ),
+        HTTPStatus.OK,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
